d55437d1fffa46d36aa1140ee80dbaae665ef287
Kumani K <kumanikidd@gmail.com>
2023-12-14 17:41:56 +0000

    types added

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,10 @@
+import SideNavbar from "../SideNavbar/SideNavbar"
+
 const Home = () => {
     return (
     <div>
         <h1>Punk Beer</h1>
+        <SideNavbar />
     </div>
     )
 }
---
+++ src/components/SideNavbar/SideNavbar.tsx
@@ -0,0 +1,11 @@
+import SearchBox from "../SearchBox/SearchBox"
+
+const SideNavbar = () => {
+    return (
+        <div className="side-navbar">
+            <SearchBox />
+        </div>
+    )
+}
+
+export default SideNavbar
