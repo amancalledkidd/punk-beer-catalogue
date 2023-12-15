6b914f3867ff599327df21f6de2935e521953217
Kumani K <kumanikidd@gmail.com>
2023-12-15 12:04:45 +0000

    better flow of data through home into main and side bar, all three components now linked, allowing search results

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import SideNavbar from "../SideNavbar/SideNavbar"
 import Main from "../Main/Main"
 import { Beer } from "../../types/types"
@@ -9,13 +10,15 @@ type HomeProps = {
 }
 
 const Home = ({ beers }: HomeProps) => {
+    const [searchBeer, setSearchBeer] = useState<string>("")
+
     return (
     <div className="home">
         <section className="home__side-navbar">
-            <SideNavbar beer={beers} />
+            <SideNavbar searchBeer={searchBeer} setSearchBeer={setSearchBeer} />
         </section>
         <section className="home__main">
-            <Main beers={beers} />
+            <Main beers={beers} searchBeer={searchBeer} />
         </section>
         
         
