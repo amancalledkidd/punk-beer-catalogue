0770bf359b54b81149dd3c36bef8656a75bb867f
Kumani K <kumanikidd@gmail.com>
2023-12-15 17:30:42 +0000

    api connected

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import Home from './components/Home/Home'
 import beers from './data/beers'
+import { Beer } from './types/types'
 import './App.scss'
 
 function App() {
+  const [punkBeers, setPunkBeers] = useState<Beer[]>([])
 
-  
+  useEffect(() => {
+    getBeers()
+  }, [])
+
+  const getBeers = async () => {
+    const response = await fetch('https://api.punkapi.com/v2/beers')
+    const data: Beer[] = await response.json();
+    setPunkBeers(data)
+    console.log(data)
+  }
+
   return (
     <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home beers={beers}/>}/>
+        <Route path="/" element={<Home beers={punkBeers}/>}/>
+        <Route path="/beers/:beerId"/>
       </Routes>
     </BrowserRouter>
     </>
